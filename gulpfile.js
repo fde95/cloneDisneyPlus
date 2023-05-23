562f36c32b52e273b6f73db50ff59c52d084e39b
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-05-23 15:44:48 -0300

    Criando o Hero e Impostando fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp'); //importando o Gulp
 const sass = require('gulp-sass')(require('sass')); //importando o Gulp-Sass e o Sass
+const imagemin = require('gulp-imagemin'); //importando o Compressor de Imagens
 
 function styles(){ //função de compressão do arquivo SASS para CSS
-    return gulp.src('./src/styles/*.scss') //recuperando os arquivos SASS
-        .pipe(sass({ outputStyle: 'compressed' })) //encadeamento de compressão
-        .pipe(gulp.dest('./dist/css')); //pasta de destido
+    return gulp.src('./src/styles/*.scss') //acessando arquivos SASS
+        .pipe(sass({ outputStyle: 'compressed' })) //comprimindo arquivo e convertendo
+        .pipe(gulp.dest('./dist/css')); //direcionando para pasta de destido
 }
 
-exports.default = styles;
+function images(){ //função de compressão de Imagens
+    return gulp.src('./src/images/**/*') //acessando a pasta de imagens
+        .pipe(imagemin()) //comprimindo imagens
+        .pipe(gulp.dest('./dist/images')); //direcionando imagens comprimidas para destido
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function(){ //função de monitoramento do Gulp
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
